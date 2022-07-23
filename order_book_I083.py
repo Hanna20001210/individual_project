70b16c6075a634491a4f076cc4ef4ecb628a8282
Hanna20001210 <72333867+Hanna20001210@users.noreply.github.com>
2022-07-23 17:41:00 +0800

    clear printout(for testing)

--- order_book_I083.py
+++ order_book_I083.py
@@ -7,7 +7,6 @@ byte_f = f.read()
 f.close()
 
 hex_f = byte_f.hex()
-#print(hex_f)
 extract_result = re.findall(r'1b3542.*?0d0a', hex_f)
 
 data_type = []
@@ -36,14 +35,11 @@ def hex_to_string(hex):
         hex = hex[2:]
     string_value = bytes.fromhex(hex).decode('utf-8')
     return string_value
-#(data[2:6] == "3541") | 
+
 for data in extract_result :
-    #print(data, len(data))
-    # & (data[38:78] == "54584f31373330304e3220202020202020202020") 
     if(data[38:78] == "54584f31373330304e3220202020202020202020"):
         for i in range(int(data[90:92])):
             data_type.append(data[2:6])
-            #msg_type.append(message_type(data[2:6]))
             info_hr.append(int(data[6:8]))
             info_min.append(int(data[8:10]))
             info_sec.append(int(data[10:12]))
@@ -57,25 +53,13 @@ for data in extract_result :
             prod_msg_seq.append(int(data[78:88]))
             cal_flag.append(hex_to_string(data[88:90]))
             no_md_entries.append(int(data[90:92]))
-            #no_md_entries.append(int(data[88:90]))
-            #md_update_action.append(hex_to_string(data[90+25*i:92+25*i]))
             md_entry_type.append(hex_to_string(data[92+24*i:94+24*i]))
             sign.append(hex_to_string(data[94+24*i:96+24*i]))
             md_entry_px.append(int(data[96+24*i:106+24*i]))
             md_entry_size.append(int(data[106+24*i:114+24*i]))
             md_entry_level.append(int(data[114+24*i:116+24*i]))
-            #print(hex_to_string(data[38:78]) , data[38:78])
-            #print(int(data[78:88]), data[78:88])
-            #print(hex_to_string(data[88:90]) , data[88:90])
-            #print(int(data[90:92]), data[90:92])
-            #print(hex_to_string(data[92+24*i:94+24*i]), data[92+24*i:94+24*i])
-            #print(hex_to_string(data[94+24*i:96+24*i]), data[94+24*i:96+24*i])
-            #print(int(data[96+24*i:106+24*i]), data[96+24*i:106+24*i])
-            #print(int(data[106+24*i:114+24*i]), data[106+24*i:114+24*i])
-            #print(int(data[114+24*i:116+24*i]), data[114+24*i:116+24*i])
             d = {
                 'data': data,
-                #'msg_type': msg_type,
                 'hr': info_hr,
                 'min': info_min,
                 'sec': info_sec,
@@ -88,7 +72,6 @@ for data in extract_result :
                 'prod_id': prod_id,
                 'prod_msg_seq': prod_msg_seq,
                 'no_md_entries':no_md_entries,
-                #'md_update_action':md_update_action,
                 'md_entry_type':md_entry_type,
                 'sign':sign,
                 'md_entry_px':md_entry_px,
@@ -96,26 +79,6 @@ for data in extract_result :
                 'md_entry_level':md_entry_level
                 }
 
-print(len(data_type))
-print(len(info_hr))
-print(len(info_min))
-print(len(info_sec))
-print(len(info_ms))
-print(len(info_us))
-print(len(channel_id ))
-print(len(channel_seq))
-print(len(ver_no ))
-print(len(body_len_list ))
-print(len(prod_id))
-print(len(prod_msg_seq ))
-print(len(cal_flag))
-print(len(no_md_entries ))
-print(len(md_update_action ))
-print(len(md_entry_type ))
-print(len(sign ))
-print(len(md_entry_px ))
-print(len(md_entry_size))
-print(len(md_entry_level))
 df = pd.DataFrame(data=d)
 
 df.to_csv("output_I083.csv")
